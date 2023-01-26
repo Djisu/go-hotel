b7ccb50194382860de52b168d7c8ac7c7c2359ad
Paul Fleischer-Djoleto <pfleischer2002@yahoo.co.uk>
2023-01-26 19:43:53 +0000

    first commit

---
+++ pkg/render/render.go
@@ -0,0 +1,98 @@
+package render
+
+import (
+	"bytes"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+
+	"github.com/Djisu/go-hotel/pkg/config"
+	"github.com/Djisu/go-hotel/pkg/models"
+)
+
+var app *config.AppConfig
+
+// NewTepmlates sets the config for the template package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+// RenderTemplate renders templates using html/templates;
+// tmpl is the name of the required template
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+
+	var tc map[string]*template.Template
+
+	if app.UseCache { // if app.UseCache == true
+		// get the template cache from the app config
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	// Getting the required template from templatecache.S
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("could not get template from template cache")
+	}
+
+	// Create buffer to hold the template to be executed.
+	buf := new(bytes.Buffer)
+
+	td = AddDefaultData(td)
+	_ = t.Execute(buf, td)
+
+	// render the template
+	_, err := buf.WriteTo(w)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	// myCache := make(map[string]*template.Template)
+	// Declare a slice to hold the templates.
+	myCache := map[string]*template.Template{}
+
+	// get all of the files named *.page.tmpl from ./templates folder
+	pages, err := filepath.Glob("./templates/*.page.tmpl")
+
+	if err != nil {
+		return myCache, err
+	}
+
+	// range through all files ending with *.page.tmpl
+	for _, page := range pages {
+		name := filepath.Base(page) // get only the part of the file path
+		ts, err := template.New(name).ParseFiles(page)
+
+		if err != nil {
+			return myCache, err
+		}
+
+		// looking for the layout.tmpl file
+		matches, err := filepath.Glob("./templates/*.layout.tmpl")
+
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 { // there are layout files.
+			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+			if err != nil {
+				return myCache, err
+			}
+
+		}
+		myCache[name] = ts
+	}
+
+	return myCache, nil
+}
